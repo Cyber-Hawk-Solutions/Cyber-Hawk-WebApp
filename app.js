ba1fc443b34305815b594b20753d725a189d6509
James Canice <cajames@abiit.internal>
2018-04-18 15:18:21 -0400

    Create homepage view from template
    Configure views to use pug templating engine. Apply homepage template to
    layout.pug and index.pug. Set root route to index.pug.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+"use strict";
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var pug = require('pug');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,17 +15,19 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.set('views',  './views');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//routes
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
