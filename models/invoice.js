26fae4309cef9c0454c97d2175c93a2f3ad3b3f9
root <root@localhost.localdomain>
2018-04-18 16:54:17 -0400

    Initalize project again

--- models/invoice.js
+++ models/invoice.js
@@ -1,1 +1,12 @@
-"";
+"use strict";
+
+let mongoose = require('mongoose');
+
+let invoiceSchema = mongoose.Schema({
+    userId: String,
+    items: [],
+    totalCost: Number,
+    period: Number
+});
+
+module.exports = mongoose.model('invoice', invoiceSchema);
--- models/user.js
+++ models/user.js
@@ -1,2 +1,12 @@
 "use strict";
 
+let mongoose = require('mongoose');
+
+let userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    company: String
+});
+
+module.exports = mongoose.model('user', userSchema);
